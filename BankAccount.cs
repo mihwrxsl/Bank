c39ede055041df9bb995bec898dbb9b415ba1428
Настя <amiheevawrxsl@gmail.com>
2025-04-22 13:33:34 +0300

    Добавьте файлы проекта.

---
+++ BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        public const string DebitAmountExceedsBalanceMessage = "Сумма снятия превышает баланс.";
+        public const string CreditAmountIsNegativeMessage = "Сумма должна быть положительной.";
+        public const string DebitAmountLessThanZeroMessage = "Сумма снятия меньше нуля.";
+
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount", amount, CreditAmountIsNegativeMessage);
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
+
+            ba.Credit(5.77);
+            try
+            {
+                ba.Debit(11.22);
+                Console.WriteLine("Current balance is ${0}", ba.Balance);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
